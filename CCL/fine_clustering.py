1d6cf2baac6775140719c9ad3a908dc27c06ba09
khanfarhan10 <njrfarhandasilva10@gmail.com>
2022-05-04 03:04:00 +0530

    CCL plus GMM nb progress error cv2

--- CCL/fine_clustering.py
+++ CCL/fine_clustering.py
@@ -1,10 +1,19 @@
 import cv2
 import numpy as np
+
+def props(img,show_uniques=False):
+    print("Shape :",img.shape,"Maximum :",img.max(),"Minimum :",img.min(),"Data Type :",img.dtype)
+    if show_uniques:
+        print("Uniques :",np.unique(img))
+
 def run_CCL_clustering(image_path = None, img_data = None, convert_BGR2RGB = True,):
+    props(img_data)
     if image_path is not None:
-        img_data = cv2.imread(image_path)
+        img_data = cv2.imread(image_path, 0)
     if convert_BGR2RGB:
         img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB)
+    img_data = img_data.astype(np.uint8) * 255
+    img_data = cv2.cvtColor(img_data, cv2.COLOR_RGB2GRAY)
     img = cv2.threshold(img_data, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
     num_labels, labels_im = cv2.connectedComponents(img)
     # return num_labels, labels_im
